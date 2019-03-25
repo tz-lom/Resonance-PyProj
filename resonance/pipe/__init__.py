c352967f4861d5535ce5cddf3c7bc5fe292edc5b
dynamite <violator86@gmail.com>
2019-03-25 18:35:46 +0300

    1. Added new tests. 2. Changed existing tests.

--- resonance/pipe/__init__.py
+++ resonance/pipe/__init__.py
@@ -1,3 +1,4 @@
 from resonance.pipe.spatial import spatial
 from resonance.pipe.filter import filter
-from resonance.pipe.nothing import nothingfrom resonance.pipe.baseline import baseline
+from resonance.pipe.nothing import nothing
+from resonance.pipe.baseline import baseline
