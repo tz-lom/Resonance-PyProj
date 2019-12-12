9998277fc6210858ecf2e9b6b2956fb79183a9ab
VAULT13\dynamite <violator86@gmail.com>
2019-12-12 18:24:35 +0300

    1. Added WindowizeByEvents processor (dev in process) 2. File names aligned

--- test/test_cross_windowize_by_event.py
+++ test/test_cross_windowize_by_event.py
@@ -6,47 +6,6 @@ from resonance.time import timeoption2ts
 import unittest
 
 
-'''
-test_that("empty data", {
-  # in
-  Csi <- SI.channels(2, 100)
-  Esi <- SI.event()
-  streams <- list(Csi, Esi)
-  blocks <- list(
-    DB.channels(Csi, timeoption2ts(Csi, 211), 26:75)
-  )
-  # out
-  Wsi <- SI.window(2, 11, 100)
-  reference <- list(out=makeEmpty(Wsi))
-  # test
-  doTest(streams, blocks, reference)
-})
-'''
-'''
-test_that("test1", {
-  # in
-  Csi <- SI.channels(1, 100)
-  Esi <- SI.event()
-  streams <- list(Csi, Esi)
-  blocks <- list(
-    DB.channels(Csi, timeoption2ts(Csi, 201), 1),
-    DB.channels(Csi, timeoption2ts(Csi, 225), 2:25),
-    DB.event(Esi, timeoption2ts(Csi, 202) , TRUE),
-    DB.event(Esi, timeoption2ts(Csi, 301) , FALSE)
-  )
-  # out
-  Wsi <- SI.window(1, 11, 100)
-  reference <- list(
-    out=DBcombine(
-      DB.window(Wsi, timeoption2ts(Csi, 212), 2:12)
-      )
-  )
-  # test
-  doTest(streams, blocks, reference)
-})
-'''
-
-
 class TestCrossWindowizeByEvent(TestProcessor):
     def test_cross_empty(self):
         c_si = si.Channels(2, 100)
