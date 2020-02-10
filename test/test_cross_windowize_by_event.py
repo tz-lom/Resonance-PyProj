79aaea4add6ee4d9c1826e0548fa7e342c9c358b
VAULT13\dynamite <violator86@gmail.com>
2020-02-10 22:35:50 +0300

    Tests for sampling signal data into windows with an offset relative to the event are added.

--- test/test_cross_windowize_by_event.py
+++ test/test_cross_windowize_by_event.py
@@ -98,3 +98,66 @@ class TestCrossWindowizeByEvent(TestProcessor):
                              self.shift,
                              late_time)
 
+    def test_data_collect_before_event(self):
+        shift = -15
+        c_si = si.Channels(self.channels, self.sampling_rate)
+        e_si = si.Event()
+
+        streams = [c_si, e_si]
+        src_blocks = [
+            db.Channels(c_si, timeoption2ts(c_si, 101), 1),
+            db.Channels(c_si, timeoption2ts(c_si, 101 + 9), np.arange(2, 11)),
+            db.Channels(c_si, timeoption2ts(c_si, 111 + 9), np.arange(11, 21)),
+            db.Channels(c_si, timeoption2ts(c_si, 121 + 9), np.arange(21, 31)),
+            db.Event(e_si, timeoption2ts(c_si, 122), "window starts with offset -15"),
+            db.Channels(c_si, timeoption2ts(c_si, 131 + 9), np.arange(31, 41))]
+
+        w_si = si.Window(self.channels, self.window_size, self.sampling_rate)
+        expected = [
+            db.Window.make_empty(w_si),
+            db.Window.make_empty(w_si),
+            db.Window.make_empty(w_si),
+            db.Window.make_empty(w_si),
+            db.Window(w_si, timeoption2ts(c_si, 121 + self.window_size + shift), np.arange(7, 18)),
+            db.Window.make_empty(w_si),
+        ]
+
+        self.check_processor(streams,
+                             src_blocks,
+                             {'out_0': expected},
+                             resonance.cross.windowize_by_events,
+                             self.window_size,
+                             shift,
+                             self.late_time)
+
+    def test_data_collect_after_event(self):
+        shift = 17
+        c_si = si.Channels(self.channels, self.sampling_rate)
+        e_si = si.Event()
+
+        streams = [c_si, e_si]
+        src_blocks = [
+            db.Channels(c_si, timeoption2ts(c_si, 101), 1),
+            db.Channels(c_si, timeoption2ts(c_si, 101 + 9), np.arange(2, 11)),
+            db.Event(e_si, timeoption2ts(c_si, 102), "window starts with offset +17"),
+            db.Channels(c_si, timeoption2ts(c_si, 111 + 9), np.arange(11, 21)),
+            db.Channels(c_si, timeoption2ts(c_si, 121 + 9), np.arange(21, 31)),
+            db.Channels(c_si, timeoption2ts(c_si, 131 + 9), np.arange(31, 41))]
+
+        w_si = si.Window(self.channels, self.window_size, self.sampling_rate)
+        expected = [
+            db.Window.make_empty(w_si),
+            db.Window.make_empty(w_si),
+            db.Window.make_empty(w_si),
+            db.Window.make_empty(w_si),
+            db.Window(w_si, timeoption2ts(c_si, 101 + self.window_size + shift), np.arange(19, 30)),
+            db.Window.make_empty(w_si),
+        ]
+
+        self.check_processor(streams,
+                             src_blocks,
+                             {'out_0': expected},
+                             resonance.cross.windowize_by_events,
+                             self.window_size,
+                             shift,
+                             self.late_time)
