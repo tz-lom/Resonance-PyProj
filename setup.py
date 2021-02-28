e33ab2e9bffbf5776ec8420865805b093c901c0a
Nuzhdin Yury <nuzhin.urii@gmail.com>
2021-02-28 14:48:08 +0100

    Remove setuptools-git-versioning dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='resonance',
-      version_config=True,
+      version='0.0.1',
       packages=find_packages(),
       url='',
       license='MIT',
       author='Yury Nuzhdin',
       author_email='nuzhdin.urii@gmail.com',
       description='',
-      install_requires=['numpy', 'scipy', 'numpy_ringbuffer'],
-      setup_requires=['setuptools-git-versioning'])
+      install_requires=['numpy', 'scipy', 'numpy_ringbuffer'])
